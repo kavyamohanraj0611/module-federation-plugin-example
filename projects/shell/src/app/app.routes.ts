2b9178f8530113e4df31802cdd943f85e7a35975
bala.veeraputhira <bala.veeraputhira@aspiresys.com>
2023-01-19 18:16:20 +0530

    Hands-on for module federation

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const APP_ROUTES: Routes = [
     {
@@ -12,10 +13,14 @@ export const APP_ROUTES: Routes = [
 
     // Your route here:
 
-    // {
-    //   path: 'flights',
-    //   loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
-    // },
+    {
+      path: 'flights',
+      loadChildren: () => loadRemoteModule({
+        type:'module',
+        remoteEntry:'http://localhost:4300/remoteEntry.js',
+        exposedModule:'./Module'
+      }).then(m => m.FlightsModule)
+    },
 
     {
       path: '**',
